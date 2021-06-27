20f997bc56c2c671539f80ba195d3fa47b473401
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-27 11:38:58 -0700

    typedefs objetcs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,49 @@
 const { gql } = require('apollo-server-express');
-const typeDefs = gql``;
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+type Auth {
+    token ID: ID!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+type BookInput {
+    bookID: String
+    authorgs: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(booKData: BookInput!):User
+    removeBook(bookId; ID!)l User
+}
+`;
+
+
+
+
 module.exports = typeDefs;
