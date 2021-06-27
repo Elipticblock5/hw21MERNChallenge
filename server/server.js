500737e4e5607c62d357927d654daf391addcd3a
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-27 13:00:03 -0700

    trying to debug bcrypt server error

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const path = require('path');
 const db = require('./config/connection');
